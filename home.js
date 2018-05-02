885df058ee32aef4952748120c2e9a99be9b20aa
Gyan Gaurav <gyan.gaurav777@gmail.com>
2018-05-02 14:40:59 +0530

    web scraping

--- home.js
+++ home.js
@@ -3,30 +3,33 @@ console.log('hello');
 const request = require('request');
 const cheerio = require('cheerio');
 const fs = require('fs');
+const express = require('express')
+const bodyParser = require('body-parser')
+ 
+const app = express()
+ 
+let url = "http://moduliweb.enac.gov.it/applicazioni/SAPR/APR_ReportOperatori.asp";
 
-var url = "http://moduliweb.enac.gov.it/applicazioni/SAPR/APR_ReportOperatori.asp";
+request(url, function (err, response, html) {
+    if(!err) {
+        const $ = cheerio.load(html);
+        let allItems = $(".container").children();
+        let items = [];
+        allItems.each(function(){
+          const result = $(".container").children().eq(index).children().eq(3).find('tr').text();
+          if(result !== ""){
+            items.push(result);
+          }
+          items.push()
+        })
+    }
 
-request(url, function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  fs.writeFile('output.json', body);
-  //console.log('body:', body); // Print the HTML for the Google homepage.
 });
 
-
-// request(url, function (err, response, html) {
-//     if(!err) {
-//         var $ = cheerio.load(html);
-//         var items = $("").children();
-//         items.each(function())
-//     }
-
-// });
-
-// fs.writeFile("output.json", JSON.stringify(items, nul, 4), function(err){
-//     if(err){
-//         console.log(err);
-//     } else {
-//         console.log('data have been added to file');
-//     }
-// })
+fs.writeFile("output.json", JSON.stringify(items, nul, 4), function(err){
+    if(err){
+        console.log(err);
+    } else {
+        console.log('data have been added to file');
+    }
+})
