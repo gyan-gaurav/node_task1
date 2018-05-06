50e252bfe332fc4167bd22f1382980d1030a8f0f
Gyan Gaurav <gyan.gaurav777@gmail.com>
2018-05-06 18:03:01 +0530

    web scraping

--- home.js
+++ home.js
@@ -3,33 +3,37 @@ console.log('hello');
 const request = require('request');
 const cheerio = require('cheerio');
 const fs = require('fs');
-const express = require('express')
-const bodyParser = require('body-parser')
- 
-const app = express()
- 
+
 let url = "http://moduliweb.enac.gov.it/applicazioni/SAPR/APR_ReportOperatori.asp";
 
-request(url, function (err, response, html) {
-    if(!err) {
-        const $ = cheerio.load(html);
-        let allItems = $(".container").children();
-        let items = [];
-        allItems.each(function(){
-          const result = $(".container").children().eq(index).children().eq(3).find('tr').text();
-          if(result !== ""){
-            items.push(result);
-          }
-          items.push()
+request(url, function (err, response, body) {
+     if(!err) {
+        const $ = cheerio.load(body);
+        const index_arr = $("#container table tbody tr th font").map((th_index, th_item)=>{ return $(th_item).text().trim() });0
+        const tr_arr = $("#container table tbody tr");
+        delete tr_arr[0];
+        const result = [];
+
+        tr_arr.each((tr_index, tr_items)=>{
+            let row = {};              
+            if(!tr_items){
+                return 
+            }
+	
+            $(tr_items).children().each((td_index, td_items)=>{
+                row[ index_arr[td_index] ] = $(td_items).text().trim()
+            })
+	        result[result.length] = row;
         })
-    }
-
-});
-
-fs.writeFile("output.json", JSON.stringify(items, nul, 4), function(err){
-    if(err){
-        console.log(err);
+    
+        fs.writeFile("output.json", JSON.stringify(result, null , " "), function(err){
+            if(err){
+                console.log(err);
+            } else {
+                console.log('data have been added to file');
+            }
+        })
     } else {
-        console.log('data have been added to file');
+        console.log(err);
     }
-})
+ });
